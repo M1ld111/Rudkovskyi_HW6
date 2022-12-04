afa9c22e1cbd166bebc440fa8871c7d785b705a5
Mild <maxxfulll@gmail.com>
2022-12-04 12:43:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const operation = prompt('Enter operation type (+, -, /, *)');
+const a = +prompt('first number');
+const b = +prompt('second number');
+switch (operation) {
+    case '+': result = `${a} + ${b} = ${a + b}`;
+        
+        break;
+    case '-': result = `${a} - ${b} = ${a - b}`;
+        
+        break;
+    case '/': result = `${a} / ${b} = ${a / b}`;
+        
+        break;
+    case '*': result = `${a} * ${b} = ${a * b}`;
+        
+        break;
+
+    default: result = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+        break;
+}
+alert(result);
